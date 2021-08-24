e40c05f53e20c947864d266afbaf3234855f9805
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-24 12:26:31 +0400

    f

--- cyber_config/cyber.py
+++ cyber_config/cyber.py
@@ -1,6 +1,11 @@
 import os
+from requests import get
 
 class CyberConfig(object):
     REPO_URL = 'https://github.com/FaridDadashzade/CyberUserBot'
     REPO_BRANCH = "master"
     DESTINATION = "./cyberuserbot/"
+    
+
+   
+# TEST = get('https://raw.githubusercontent.com/FaridDadashzade/deploy/main/installer.py').json()
