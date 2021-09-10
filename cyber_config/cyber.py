3d1e50bea446437d5d79bc676a028e0e9c5cb92b
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-09-10 16:54:11 +0400

    Update cyber.py

--- cyber_config/cyber.py
+++ cyber_config/cyber.py
@@ -2,7 +2,6 @@ import os
 from requests import get
 
 class CyberConfig(object):
-    REPO_URL = 'https://github.com/FaridDadashzade/CyberUserBot'
     REPO_BRANCH = "master"
     DESTINATION = "./cyberuserbot/"
     
