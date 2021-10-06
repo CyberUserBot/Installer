d53233aecc39720ccdcd5775cf24203ecb547609
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-10-06 20:58:36 +0400

    fix

--- cyber_installer/__main__.py
+++ cyber_installer/__main__.py
@@ -82,6 +82,7 @@ async def botlog (String, Api, Hash):
 
 if __name__ == "__main__":
     logo(LANGUAGE)
+    loop = asyncio.get_event_loop()
     api = soru(LANG['HEROKU_KEY'])
     bilgi(LANG['HEROKU_KEY_LOGIN'])
     heroku = connect(api)
